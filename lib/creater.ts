0f4a307dd121e1a47daf0ff562853427bcab5296
Paul Maddox <paul.maddox@gmail.com>
2019-12-10 20:33:22 -0800

    Added a 2TB EBS volume

--- lib/creater.ts
+++ lib/creater.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
 import autoscaling = require('@aws-cdk/aws-autoscaling');
 import { Vpc, AmazonLinuxImage, AmazonLinuxGeneration } from '@aws-cdk/aws-ec2';
+import { BlockDeviceVolume } from '@aws-cdk/aws-autoscaling';
 
 export interface CraterProps {
     vpc: ec2.IVpc,
@@ -14,6 +15,7 @@ export class Crater extends cdk.Construct {
     constructor(scope: cdk.Construct, id: string, props: CraterProps) {
         super(scope, id);
 
+        // Create a single EC2 instance, in an auto scaling group
         const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
             vpc: props.vpc,
             keyName: props.keyName,
@@ -21,9 +23,21 @@ export class Crater extends cdk.Construct {
             machineImage: new AmazonLinuxImage({
                 generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
             }),
+            blockDevices: [{
+                deviceName: "/dev/sda1",
+                volume: BlockDeviceVolume.ebs(2000),
+            }]
         })
 
+        // Allow SSH from anywhere
         asg.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
+
+        // Setup the instance ready for crater
+        asg.addUserData("yum update -y");
+        asg.addUserData("yum install -y git docker");
+        asg.addUserData("systemctl enable docker.service");
+        asg.addUserData("systemctl start docker.service");
+        asg.addUserData("docker pull rustops/crates-build-env:latest");
 
     }
 
