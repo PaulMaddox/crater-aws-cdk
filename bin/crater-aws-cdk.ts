7a57ce284401bc3a74a423b92827b1174bdccc5d
Paul Maddox <paul.maddox@gmail.com>
2019-12-10 20:00:26 -0800

    Added default account

--- bin/crater-aws-cdk.ts
+++ bin/crater-aws-cdk.ts
@@ -4,4 +4,9 @@ import cdk = require('@aws-cdk/core');
 import { CraterAwsCdkStack } from '../lib/crater-aws-cdk-stack';
 
 const app = new cdk.App();
-new CraterAwsCdkStack(app, 'CraterAwsCdkStack');
+new CraterAwsCdkStack(app, 'CraterAwsCdkStack', {
+    env: {
+        account: "676592238803",
+        region: "eu-west-1",
+    }
+});
